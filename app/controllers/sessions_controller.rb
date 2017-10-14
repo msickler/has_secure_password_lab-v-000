a801f658c1b1a388184679eebe9fe76162cace3a
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-14 00:43:53 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,14 @@
 class SessionsController < ApplicationController
 
-  def login 
+  def login
     render :'users/login'
-  end 
-
-  def create
-    @user = User.find_by(name: params[:name])
-    return head(:forbidden) unless @user.authenticate(params[:password])
-    session[:user_id] = @user.id
   end
 
-end 
+  def create
+    @user = User.find_by(name: params[:user][:name])
+    return head(:forbidden) unless @user.authenticate(params[:user][:password])
+    session[:user_id] = @user.id
+    redirect_to '/'
+  end
+
+end
