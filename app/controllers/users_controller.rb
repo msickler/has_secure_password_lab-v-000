4094b0a15a5a1035b564986b7f93f64c6c716d2b
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-14 00:33:27 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,11 @@
 class UsersController < ApplicationController
+  def create 
+    user = User.new(user_params).save
+  end 
+
+  private 
+
+  def user_params
+    params.require(:user).permit(:name, :password, :password_confirmation)
+  end 
 end
