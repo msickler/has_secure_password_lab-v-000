5da6deb5a49fc8ed4bbad772f55289909f3acc03
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-14 00:54:10 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,13 +5,13 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params)
-    if @user.save
-      session[:user_id] = @user.id
-      redirect_to '/'
+    user = User.new(user_params)
+    if user.save
+      session[:user_id] = user.id    
+      redirect_to "/"
     else
-    redirect_to '/new'
-  end
+      redirect_to "/users/new"
+    end
   end
 
   def home
